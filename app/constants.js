5b7247e1857454af3c4363268f6a08db2881d768
Lucas Oliveira <lcsoliveira@ibm.com>
2020-11-09 21:27:55 -0300

    add contacts constant

--- app/constants.js
+++ app/constants.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 //API URL
-export const API_URL = 'https://mesannodejsapiwithverification.herokuapp.com/api';
+// export const API_URL = 'https://mesannodejsapiwithverification.herokuapp.com/api';
+export const API_URL = 'http://localhost:3000/api';
 
 //API End Points
 export const REGISTER = `${API_URL}/auth/register`;
 export const LOGIN = `${API_URL}/auth/login`;
 export const UPDATE_PROFILE = `${API_URL}/user`;
 export const UPLOAD_IMAGE = `${API_URL}/user/upload`;
+export const CONTACTS = `${API_URL}/user/contacts`;
 export const FORGOT_PASSWORD = `${API_URL}/auth/recover`;
