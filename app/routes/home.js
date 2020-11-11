50802d20550d0e31d63dda0286a0cdaf06bf9f35
Lucas Oliveira <lcsoliveira@ibm.com>
2020-11-11 14:30:45 -0300

    map scenes import

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,6 +4,7 @@ import {createStackNavigator} from 'react-navigation-stack';
 //IMPORT SCENES
 import HomeScreen from "../scenes/home/Home";
 import UpdateProfileScreen from "../scenes/home/UpdateProfile";
+import Map from "../scenes/Map/Map";
 
 import {headerStyle, headerTitleStyle} from '../theme'
 
@@ -11,6 +12,7 @@ const HomeStack = createStackNavigator(
     {
         Home: HomeScreen,
         UpdateProfile: UpdateProfileScreen,
+        Map: Map
     },
     {
         initialRouteName: 'Home',
